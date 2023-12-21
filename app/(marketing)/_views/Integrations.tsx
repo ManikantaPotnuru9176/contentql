1cfab0a8ed8c6d6dee38e42ba7e2b6bc2e13debd
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-12-21 20:21:15 +0530

    updated the integration section

--- app/(marketing)/_views/Integrations.tsx
+++ app/(marketing)/_views/Integrations.tsx
@@ -17,6 +17,26 @@ const Integrations = () => {
       "assets/logos/whatsapp.svg",
       "assets/logos/twitter.svg",
     ],
+    cards: [
+      {
+        name: "Slack",
+        description:
+          "Streamline project discussions, share updates, and boost real-time collaboration effortlessly.",
+        logo: "assets/logos/slack.svg",
+      },
+      {
+        name: "Google Drive",
+        description:
+          "File management, Enhance collaboration, and Elevate your productivity with the convenience of this integrated solution",
+        logo: "assets/logos/google-drive.svg",
+      },
+      {
+        name: "Bitbucket",
+        description:
+          "Streamline version control, collaboration, and project management with ease. Embrace the power of seamless GitHub integration.",
+        logo: "assets/logos/bitbucket.svg",
+      },
+    ],
   };
 
   return (
@@ -46,7 +66,7 @@ const Integrations = () => {
                     <img
                       src={logo}
                       alt={`Logo ${index + 1}`}
-                      className="w-8 h-8 object-cover"
+                      className="w-10 h-10 object-cover"
                     />
                   </div>
                 ))}
@@ -57,18 +77,68 @@ const Integrations = () => {
                 .map((logo, index) => (
                   <div
                     key={index}
-                    className="rounded-full bg-white p-4 overflow-hidden"
+                    className="rounded-full bg-white p-4 overflow-hidden shadow-lg"
                   >
                     <img
                       src={logo}
                       alt={`Logo ${index + 1}`}
-                      className="w-8 h-8 object-cover"
+                      className="w-10 h-10 object-cover"
                     />
                   </div>
                 ))}
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex flex-row space-x-6 pt-6">
+        {integrations.cards.map((card, index) => (
+          <div
+            key={index}
+            className="card rounded w-96 bg-base-100 shadow-sm border"
+          >
+            <div className="card-body p-4">
+              <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row">
+                  <div
+                    key={index}
+                    className="rounded-full bg-base-200 p-2 overflow-hidden"
+                  >
+                    <img
+                      src={card.logo}
+                      alt={`Logo ${index + 1}`}
+                      className="w-8 h-8 object-cover"
+                    />
+                  </div>
+                  <h2 className="pl-4 card-title text-lg font-semibold">
+                    {card.name}
+                  </h2>
+                </div>
+                <div className="flex justify-center items-center">
+                  <div className="rounded-full bg-base-200 p-2 self-end">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 512 512"
+                    >
+                      <g transform="rotate(90 256 256)">
+                        <path
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="48"
+                          d="m112 244l144-144l144 144M256 120v292"
+                        />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <p className="text-sm">{card.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
